e86852375d4322a1723124f7d45a27aafa3621e5
Tushar Saini <ts042049@gmail.com>
2026-09-24 21:44:14 +0530

    Add grade calculation based on percentage in day 3 task

--- day3.py
+++ day3.py
@@ -10,3 +10,17 @@ percentage = total / 5
 print("Total Marks:", total)
 print("Percentage:", percentage, "%")
 
+if percentage >= 90:
+    grade = "A+"
+elif percentage >= 80:
+    grade = "A"
+elif percentage >= 70:
+    grade = "B"
+elif percentage >= 60:
+    grade = "C"
+elif percentage >= 50:
+    grade = "D"
+else:
+    grade = "F"
+
+print("Grade:", grade)
